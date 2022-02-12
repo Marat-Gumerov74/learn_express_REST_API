6ed565a744eadf6b248c100684661df6971ae7fa
Marat Gumerov <gumerovm74@gmail.com>
2022-02-12 10:17:30 +0300

    add methods crud

--- rest-api/routes/books.js
+++ rest-api/routes/books.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const { v4: uuidv4 } = require('uuid');
 
 let books = [{
     id: 1,
@@ -16,9 +17,46 @@ router.get('/:id', (req, res) =>{
     if (book) {
         return res.json(book);
     } else {
-        return res.status(404)
-                .json({status: 'Not found'})
+        return res
+        .status(404)
+        .json({status: 'Not found'})
     }
 });
+
+router.post('/', (req, res) => {
+    const book = {
+        title: req.body.title || 'Default title',
+        author: req.body.author || 'Default autor',
+        id: uuidv4()
+    }
+    books.push(book);
+    return res.json(book);
+});
+
+router.put('/:id', (req, res) => {
+    const bookId = parseInt(req.params.id, 10);
+    books.forEach((book) => {
+        if(book.id === bookId) {
+            book.title = req.body.title;
+            book.author = req.body.author;
+        }
+    });
+    const newBook = books.find(book => book.id === bookId);
+    return res.json(newBook);
+});
+
+router.delete('/:id', (req,res) => {
+    const bookId = parseInt(req.params.id, 10);
+    
+    books = books.filter(book => book.id != book.bookId );
+    
+    const existBook = books.find(book => book.id === bookId);
+    
+    if(!existBook){
+        return res.send(`Book with ${bookId} was deleted`).status(200);
+    } else {
+        return res.send(`something wrong`).status(400);
+    }
+})
 
 module.exports = router;
